e29e7e08077f0f20699c58b18b3dd8ecc3ea213b
SRV <sauravextra@gmail.com>
2026-04-18 19:36:02 +0530

    Initial Spring boot project with web,jpa,postgres sql driver

---
+++ src/main/java/org/example/notificationservicev2/NotificationServiceV2Application.java
@@ -0,0 +1,13 @@
+package org.example.notificationservicev2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotificationServiceV2Application.class, args);
+	}
+
+}
