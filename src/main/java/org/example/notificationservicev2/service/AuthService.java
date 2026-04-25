de775fa10db8c56565ebebbe455e49a8527121c2
SRV <sauravextra@gmail.com>
2026-04-25 18:19:17 +0530

    Now we can register user as well and login too throw./auth endpoint

--- src/main/java/org/example/notificationservicev2/service/AuthService.java
+++ src/main/java/org/example/notificationservicev2/service/AuthService.java
@@ -6,10 +6,13 @@ import lombok.RequiredArgsConstructor;
 import org.example.notificationservicev2.dto.RegisterRequest;
 import org.example.notificationservicev2.entity.User;
 import org.example.notificationservicev2.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -17,12 +20,13 @@ import org.springframework.stereotype.Service;
 public class AuthService {
     private final UserRepository userRepository;
     private final AuthenticationManager authenticationManager;
+    private final PasswordEncoder passwordEncoder;
     public User authRegister (RegisterRequest request) {
         if(userRepository.existsByEmail(request.getEmail())){
             //return UsernameNotFoundException;
         }
         User user = User.builder().email(request.getEmail()).
-                password(request.getPassword()).build();
+                password(passwordEncoder.encode(request.getPassword())).build();
         userRepository.save(user);
         return user;
     }
