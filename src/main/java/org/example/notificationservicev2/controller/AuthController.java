bd1b3855802a7becea23806c1572afd4311dd43b
SRV <sauravextra@gmail.com>
2026-04-25 18:35:38 +0530

    Side Quest -> now we have normal spring security session running no JWT just register login and you can access other end points. Will replace this JWT now.

--- src/main/java/org/example/notificationservicev2/controller/AuthController.java
+++ src/main/java/org/example/notificationservicev2/controller/AuthController.java
@@ -1,10 +1,13 @@
 package org.example.notificationservicev2.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.example.notificationservicev2.dto.LoginResponseDTO;
 import org.example.notificationservicev2.dto.RegisterRequest;
 import org.example.notificationservicev2.entity.User;
 import org.example.notificationservicev2.service.AuthService;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,8 +26,12 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public LoginResponseDTO login(@RequestBody RegisterRequest user) {
+    public LoginResponseDTO login(@RequestBody RegisterRequest user, HttpServletRequest request) {
         User response =  authService.authLogin(user);
+        request.getSession().setAttribute(
+                HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY,
+                SecurityContextHolder.getContext()
+        );
         return new LoginResponseDTO("token",response.getEmail(), response.getId());
 
     }
--- src/main/java/org/example/notificationservicev2/service/AuthService.java
+++ src/main/java/org/example/notificationservicev2/service/AuthService.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -37,6 +38,11 @@ public class AuthService {
         );
 
         User foundInDB = (User) authentication.getPrincipal();
+
+        SecurityContextHolder.getContext()
+                .setAuthentication(authentication);
+
+
 
         return foundInDB;
     }
